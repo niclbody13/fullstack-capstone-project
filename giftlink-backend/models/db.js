f5d03cbfee11c30cb6f81962fcc6a038650100bd
niclbody13 <91341366+niclbody13@users.noreply.github.com>
2025-07-03 17:36:19 -0700

    update files

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -13,15 +13,11 @@ async function connectToDatabase() {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
     await client.connect();
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    dbInstance  = client.db(dbName)
-
-    // Task 3: Return database instance
-    return dbInstance;}
+    dbInstance = client.db(dbName);
+    return dbInstance;
+}
 
 module.exports = connectToDatabase;
